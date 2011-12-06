janosvitok/ffado

commit 994d921432679b1a4f2339512257cab69741814a

motu: allocate the send iso channel before the receive channel to see if the 828mk1 - as currently set up - is sensitive to the precise channels allocated.  If an 828mk1 is in use, show the iso control register value as read from the device for debug purposes.
git-svn-id: http://subversion.ffado.org/ffado@2012 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/motu/motu_avdevice.cpp
@@ -1463,11 +1463,11 @@ MotuDevice::prepare() {
     m_tx_bandwidth = 25 + (n_events_per_packet*event_size_out);
 
     // Assign iso channels if not already done
-    if (m_iso_recv_channel < 0)
-        m_iso_recv_channel = get1394Service().allocateIsoChannelGeneric(m_rx_bandwidth);
-
     if (m_iso_send_channel < 0)
         m_iso_send_channel = get1394Service().allocateIsoChannelGeneric(m_tx_bandwidth);
+
+    if (m_iso_recv_channel < 0)
+        m_iso_recv_channel = get1394Service().allocateIsoChannelGeneric(m_rx_bandwidth);
 
     debugOutput(DEBUG_LEVEL_VERBOSE, "recv channel = %d, send channel = %d\n",
         m_iso_recv_channel, m_iso_send_channel);

commit 815acdcb895cc70c97029f8dc915fc5a48db323b

motu: fix a typo introduced in r2012
git-svn-id: http://subversion.ffado.org/ffado@2013 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/motu/motu_avdevice.cpp
@@ -1686,6 +1686,7 @@ quadlet_t isoctrl = ReadRegister(MOTU_REG_ISOCTRL);
          * At this point also make sure that two additional bits, which
          * appear to be I/O enable bits, are set.
          */
+debugOutput(DEBUG_LEVEL_VERBOSE, "MOTU g1: read isoctl: %x\n", isoctrl);
         isoctrl &= ~MOTU_G1_C1_ISO_INFO_MASK;
         isoctrl |= (MOTU_G1_C1_ISO_TX_ACTIVE | MOTU_G1_C1_ISO_TX_WREN |
                     MOTU_G1_C1_ISO_RX_ACTIVE | MOTU_G1_C1_ISO_RX_WREN);

trunk/libffado/support/mixer-qt4/ffado/widgets/matrixmixer.py
@@ -96,10 +96,10 @@ class MixerNode(QtGui.QAbstractSlider):
             self.addAction(action)
 
     def directValues(self,text):
-        #log.debug("MixerNode.directValues( '%s' )" % text)
+        log.debug("MixerNode.directValues( '%s' )" % text)
         text = text.split(" ")[0].replace(",",".")
         n = pow(10, (float(text)/20)) * pow(2,14)
-        #log.debug("%g" % n)
+        log.debug("%g" % n)
         self.setValue(n)
 
     def mousePressEvent(self, ev):