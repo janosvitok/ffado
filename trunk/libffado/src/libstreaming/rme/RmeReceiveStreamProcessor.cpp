janosvitok/ffado

commit f2c26646e279ad29b35ebd6d15d65919f8a63db3

rme: arrange to timestamp incoming frames a little more realistically.
git-svn-id: http://subversion.ffado.org/ffado@2259 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/libstreaming/rme/RmeReceiveStreamProcessor.cpp
@@ -173,12 +173,12 @@ if (rep == 0) {
         // this stuffs up the rx DLL since the resulting instantaneous
         // sample rate is extremely high.
         //
-        // Finally the packet timestamp is "future dated" by a set number
-        // of cycles, determined experimentally to give the most reliable
-        // results.
+        // Finally the packet timestamp is "back dated" by a the rx
+        // transfer delay to account for delays between the ADCs and 
+        // reception of the audio data.
 
         pkt_timestamp = CYCLE_TIMER_TO_TICKS(CYCLE_TIMER_SET_OFFSET(pkt_ctr, 
-                          0)) + 3*TICKS_PER_CYCLE;
+                          0)) - RME_RECEIVE_TRANSFER_DELAY;
         if (pkt_timestamp < 0)
           pkt_timestamp += 128LL*TICKS_PER_SECOND;
         else

trunk/libffado/config.h.in
@@ -215,10 +215,11 @@
 // -- RME options -- //
 
 // the transfer delay is substracted from the ideal presentation
-// time to obtain a corrected presentation time. This values is
+// time to obtain a corrected presentation time. This value is
 // then used to stamp the packet and determine the transmission
-// time instant.
-#define RME_TRANSMIT_TRANSFER_DELAY    (0U)
+// time instant.  This is in ticks, and has been determined 
+// experimentally to provide the most stability.
+#define RME_TRANSMIT_TRANSFER_DELAY    (3U*TICKS_PER_CYCLE)
 
 // the absolute minimum number of cycles we want to transmit
 // a packet ahead of the presentation time. The nominal time
@@ -226,14 +227,19 @@
 // given by RME_TRANSMIT_TRANSFER_DELAY (in ticks), but in case we
 // are too late for that, this constant defines how late we can
 // be.
-#define RME_MIN_CYCLES_BEFORE_PRESENTATION                0
+#define RME_MIN_CYCLES_BEFORE_PRESENTATION                1
 
 // the absolute maximum number of cycles we want to transmit
 // a packet ahead of the ideal transmit time. The nominal time
 // the packet is transmitted ahead of the presentation time is
 // given by RME_TRANSMIT_TRANSFER_DELAY (in ticks), but we can send
 // packets early if we want to.
-#define RME_MAX_CYCLES_TO_TRANSMIT_EARLY                  1
+#define RME_MAX_CYCLES_TO_TRANSMIT_EARLY                  5
+
+// The transfer delay assumed for incoming packets from an RME. 
+// Normally this would be the same as the transmit transfer delay.
+// This value is in ticks.
+#define RME_RECEIVE_TRANSFER_DELAY     RME_TRANSMIT_TRANSFER_DELAY
 
 // ensure that the RME tx SP clips all float values to [-1.0..1.0]
 #define RME_CLIP_FLOATS                                   1

commit 16cd6b2f852b9ea3814feec4a6173698f4fab391

rme: fix an arithmetic signedness problem introduced in r2259 when a subtraction was added to the timestamp calculation.  The error caused erroneous timestamps to be calculated which lead to apparent xruns roughly once every 128 seconds (the timestamp wrap value).
git-svn-id: http://subversion.ffado.org/ffado@2270 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/libstreaming/rme/RmeReceiveStreamProcessor.cpp
@@ -178,7 +178,8 @@ if (rep == 0) {
         // reception of the audio data.
 
         pkt_timestamp = CYCLE_TIMER_TO_TICKS(CYCLE_TIMER_SET_OFFSET(pkt_ctr, 
-                          0)) - RME_RECEIVE_TRANSFER_DELAY;
+                          0));
+        pkt_timestamp -= (int64_t)RME_RECEIVE_TRANSFER_DELAY;
         if (pkt_timestamp < 0)
           pkt_timestamp += 128LL*TICKS_PER_SECOND;
         else