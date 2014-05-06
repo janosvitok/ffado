janosvitok/ffado

commit 5543b6b9025421a31ed4fe26c4811d940c1b4f30

SConstruct: further refinement of debug control at build time.  A new DEBUG_MESSAGES option controls whether debug messages are printed (default is enabled).  DEBUG now turns on warnings, symbols and additional debug code (default is enabled).  If DEBUG is not enabled a '-O2' build is done.  The DEBUG_BUILD option added in r2507 has been removed.  Production builds are encouraged to leave DEBUG_MESSAGES enabled while disabling DEBUG.
git-svn-id: http://subversion.ffado.org/ffado@2517 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/debugmodule/debugmodule.h
@@ -116,8 +116,8 @@ typedef short debug_level_t;
                                      ##args )
 
 // these are for messages that are also displayed when not compiled
-// for debug
-#ifdef DEBUG
+// for debug messages
+#ifdef DEBUG_KESSAGES
 #define printMessage( format, args... )                             \
                 m_debugModule.print( DebugModule::eDL_Message,      \
                                      __FILE__,                      \
@@ -191,7 +191,7 @@ typedef short debug_level_t;
 
 #endif
 
-#ifdef DEBUG
+#ifdef DEBUG_MESSAGES
 
     #define debugOutput( level, format, args... )                  \
                 m_debugModule.print( level,                        \

trunk/libffado/SConstruct
@@ -39,9 +39,11 @@ if not os.path.isdir( "cache" ):
 opts = Variables( "cache/options.cache" )
 
 opts.AddVariables(
-    BoolVariable( "DEBUG_BUILD", "Build with \"-g -Wall\" rather than \"-O2\"", True ),
-    BoolVariable( "DEBUG", "Enable support for debug messages", True ),
-    BoolVariable( "PROFILE", "Build with symbols and other profiling info", False ),
+    BoolVariable( "DEBUG", """\
+Build with \"-g -Wall\" rather than \"-O2\", and include extra debugging
+checks in the code.""", True ),
+    BoolVariable( "DEBUG_MESSAGES", "Enable support for debug messages", True ),
+    BoolVariable( "PROFILE", "Build with symbols, warnings and other profiling info", False ),
     PathVariable( "PREFIX", "The prefix where ffado will be installed to.", "/usr/local", PathVariable.PathAccept ),
     PathVariable( "BINDIR", "Overwrite the directory where apps are installed to.", "$PREFIX/bin", PathVariable.PathAccept ),
     PathVariable( "LIBDIR", "Overwrite the directory where libs are installed to.", "$PREFIX/lib", PathVariable.PathAccept ),
@@ -403,16 +405,14 @@ config_guess = conf.ConfigGuess()
 
 env = conf.Finish()
 
-if env['DEBUG_BUILD']:
+if env['DEBUG']:
     print "Doing a debug build"
-    env.MergeFlags( "-Wall -g" )
+    env.MergeFlags( "-Wall -g -DDEBUG" )
 else:
     env.MergeFlags( "-O2" )
 
-if env['DEBUG']:
-    env.MergeFlags( "-DDEBUG" )
-else:
-    env.MergeFlags( "-DNDEBUG" )
+if env['DEBUG_MESSAGES']:
+    env.MergeFlags( "-DDEBUG_MESSAGES" )
 
 if env['PROFILE']:
     print "Doing a PROFILE build"

trunk/libffado/src/debugmodule/debugmodule.cpp
@@ -43,7 +43,7 @@
 
 #if DEBUG_USE_MESSAGE_BUFFER
 #else
-    #ifdef DEBUG
+    #ifdef DEBUG_MESSAGES
         #warning Printing debug info without ringbuffer, not RT-safe!
     #else
         #error Printing debug info without ringbuffer, not RT-safe (not allowed for non-debug builds)!

commit 0463e98e9506b4e74a4a3aa808c96ebe3578ba6c

Correct a typo introduced in r2517.  Thanks to Jano Svitok for spotting this.
git-svn-id: http://subversion.ffado.org/ffado@2520 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/debugmodule/debugmodule.h
@@ -117,7 +117,7 @@ typedef short debug_level_t;
 
 // these are for messages that are also displayed when not compiled
 // for debug messages
-#ifdef DEBUG_KESSAGES
+#ifdef DEBUG_MESSAGES
 #define printMessage( format, args... )                             \
                 m_debugModule.print( DebugModule::eDL_Message,      \
                                      __FILE__,                      \