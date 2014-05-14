janosvitok/ffado

commit e6c3b865535e1eb80757776cde080e8616a16af2

m-audio: fix uninitialised variable, spotted by Andreas Hehn.
git-svn-id: http://subversion.ffado.org/ffado@2521 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/bebob/maudio/special_avdevice.cpp
@@ -213,6 +213,7 @@ bool Device::writeReg(uint64_t offset, uint32_t data)
     /* cache because it's 'write-only' */
     m_regs[offset / 4] = data;
 
+    trials = 0;
     do {
         if (get1394Service().write_quadlet(nodeId, addr, quad))
             break;
@@ -238,6 +239,7 @@ bool Device::writeBlk(uint64_t offset, unsigned int size, uint32_t *data)
         data[i] = CondSwapToBus32(data[i]);
     }
 
+    trials = 0;
     do {
         if (get1394Service().write(nodeId, addr, length, (fb_quadlet_t*)data))
             break;

commit c8052f519f96c5b9b453615c40e308a252bbbf48

m-audio: remove a rogue extended character introduced inadvertently in r2521.  It went unnoticed because it displayed as a space in my editor.
git-svn-id: http://subversion.ffado.org/ffado@2529 2be59082-3212-0410-8809-b0798e1608f0

trunk/libffado/src/bebob/maudio/special_avdevice.cpp
@@ -239,7 +239,7 @@ bool Device::writeBlk(uint64_t offset, unsigned int size, uint32_t *data)
         data[i] = CondSwapToBus32(data[i]);
     }
 
-    trials = 0;
+    trials = 0;
     do {
         if (get1394Service().write(nodeId, addr, length, (fb_quadlet_t*)data))
             break;